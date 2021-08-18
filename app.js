54c21683496a5de3747a253a4a109e77d75d1399
Michael Scott Williamson <michael.williamson@tamucc.edu>
2021-08-18 11:32:45 -0500

    Now returns folders and only folders in user/incoming.

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const fs = require('fs');
+
 
 app.get('/getFolders/:user', (req, res) => {
   var user = req.params.user;
-  res.json({ "hello": user });
+
+  fs.readdir(user + "/incoming/", 
+    { withFileTypes: true },
+    (err, files) => {
+    console.log("\nCurrent directory files:");
+    let dirArry = new Array;
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        if(file.isDirectory()) {
+          dirArry.push(file.name);
+          console.log('pushed ' + file.name);
+        }
+      })
+    }
+    res.json([dirArry]);
+  })
 })
 
 app.listen(port, () => {
   console.log(`Folder getter app listening at http://localhost:${port}`)
-})
+});
