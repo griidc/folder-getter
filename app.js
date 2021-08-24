3515da68128b2fcb5fe23fb346297a6831a384c1
Michael Scott Williamson <michael.williamson@tamucc.edu>
2021-08-24 13:04:39 -0500

    Add eslint, clean up some code.

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 const fs = require('fs');
 
-
 app.get('/getFolders/:user', (req, res) => {
-  var user = req.params.user;
+  const {user} = req.params;
 
-  fs.readdir(user + "/incoming/", 
+  fs.readdir(`${user}/incoming/`,
     { withFileTypes: true },
     (err, files) => {
-    console.log("\nCurrent directory files:");
-    let dirArry = new Array;
-    if (err)
-      console.log(err);
-    else {
-      files.forEach(file => {
-        if(file.isDirectory()) {
-          dirArry.push(file.name);
-          console.log('pushed ' + file.name);
-        }
-      })
-    }
-    res.json([dirArry]);
-  })
+      // eslint-disable-next-line no-console
+      console.log('\nCurrent directory files:');
+      const dirArry = new Array;
+      if (err)
+        // eslint-disable-next-line nonblock-statement-body-position
+        console.log(err);
+      else {
+        files.forEach(file => {
+          if(file.isDirectory()) {
+            dirArry.push(file.name);
+            console.log('pushed ' + file.name);
+          }
+        })
+      }
+      res.json([dirArry]);
+    })
 })
 
 app.listen(port, () => {
