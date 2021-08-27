fd12d8ca86d92a7a3906a176d5a709fcf33d8cfd
Michael Scott Williamson <michael.williamson@tamucc.edu>
2021-08-27 09:25:30 -0500

    restructured code. Added SSL support.

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const https = require('https');
 const fs = require('fs');
+const { Http2ServerRequest } = require('http2');
+
+const port = 3000;
+const sslMode = false;
+
+const privateKeyFile = '';
+const sslCertFile = '';
+
+const app = express();
 
 app.get('/getFolders/:user', (req, res) => {
-  const {user} = req.params;
+  const { user } = req.params;
 
   fs.readdir(`${user}/incoming/`,
     { withFileTypes: true },
     (err, files) => {
-      // eslint-disable-next-line no-console
-      console.log('\nCurrent directory files:');
-      const dirArry = new Array;
-      if (err)
-        // eslint-disable-next-line nonblock-statement-body-position
+      const dirArry = [];
+      if (err) {
+        // eslint-disable-next-line no-console
         console.log(err);
-      else {
-        files.forEach(file => {
-          if(file.isDirectory()) {
+      } else {
+        files.forEach((file) => {
+          if (file.isDirectory()) {
             dirArry.push(file.name);
-            console.log('pushed ' + file.name);
           }
-        })
+        });
       }
       res.json([dirArry]);
-    })
-})
-
-app.listen(port, () => {
-  console.log(`Folder getter app listening at http://localhost:${port}`)
+    });
 });
+
+if (sslMode === true) {
+  const privateKey = fs.readFileSync(privateKeyFile);
+  const sslCertificate = fs.readFileSync(sslCertFile);
+
+  https.createServer({
+    key: privateKey,
+    cert: sslCertificate
+  }, app).listen(port);
+  // eslint-disable-next-line no-console
+  console.log(`Folder getter app listening at https://localhost:${port}`);
+} else {
+  app.listen(port, () => {
+  });
+  // eslint-disable-next-line no-console
+  console.log(`Folder getter app listening at http://localhost:${port}`);
+}
